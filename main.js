fa0278710fd9a93279ca4e08164bdc5b1381e7b5
faulus-RC <Chris.Rosenberg@ros-con.com>
2025-08-09 12:24:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -546,6 +546,3 @@ if ("serviceWorker" in navigator) {
     window.location.reload();
   });
 }
-
-const APP_VERSION = "v6";
-document.getElementById("version").textContent = "Version: " + APP_VERSION;
